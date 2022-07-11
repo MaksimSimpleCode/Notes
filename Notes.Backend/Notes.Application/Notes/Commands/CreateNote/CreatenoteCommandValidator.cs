dc86afb1a46610c214e7507bae9beaa8f21fcf9d
Maksim Gusev <gusev@samarasoft.com>
2022-07-11 13:45:53 +0400

    FluentValidation

---
+++ Notes.Backend/Notes.Application/Notes/Commands/CreateNote/CreatenoteCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentValidation;
+
+namespace Notes.Application.Notes.Commands.CreateNote
+{
+    //Валидация с помощью FluentValidationю
+    //По сути просто клас, в конструктре которого задаем правила для нужного класса.
+    public class CreatenoteCommandValidator : AbstractValidator<CreateNoteCommand>
+    {
+        public CreatenoteCommandValidator()
+        {
+            RuleFor(createNoteCommand => createNoteCommand.Title).NotEmpty().MaximumLength(250);
+            RuleFor(createNoteCommand => createNoteCommand.UserId).NotEqual(Guid.Empty);
+        }
+
+    }
+}
---
+++ Notes.Backend/Notes.Application/Notes/Commands/DeleteCommand/DeletenoteCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FluentValidation;
+
+namespace Notes.Application.Notes.Commands.DeleteCommand
+{
+    public class DeletenoteCommandValidator : AbstractValidator<DeleteNoteCommand>
+    {
+        public DeletenoteCommandValidator()
+        {
+            RuleFor(updateNoteCommand => updateNoteCommand.Id).NotEqual(Guid.Empty);
+            RuleFor(updateNoteCommand => updateNoteCommand.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
